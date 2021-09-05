7e6a29866acf7d89a9df9956de047d0c11989a75
mnubi <rodger.kamau@student.moringaschool.com>
2021-09-05 15:49:52 +0300

    created an object to give a specific day of the date entered

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ var submit = function(){
     var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-    //fetching data from HTML using their id
+    //fetching input data from HTML using their id
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
     var day = parseInt(document.getElementById("day").value);
     var name =(document.getElementById("name").value);
 
+    //creating a date objects to call the day stated
+    var date0fbirth = new Date(year + "/" + month + "/" + day);
+    var results = date0fbirth.getDay();
+
+    
 };
