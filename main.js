b45e7d16c733b87b51e7abac3f37bfdd41723829
mnubi <rodger.kamau@student.moringaschool.com>
2021-09-05 15:35:01 +0300

    get element from html by id and name to js

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
 //possible variables for birth date corresponds with day of the week
 var submit = function(){
-    var DaysofTheWeek = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-    ]
-    var maleNames = [
-        "Kwasi",
-        "Kwadwo",
-        "Kwabena",
-        "Kwaku",
-        "Yaw",
-        "Kofi",
-        "Kwame"
-      ];
-      var femaleNames = [
-        "Akosua",
-        "Adwoa",
-        "Abenaa",
-        "Akua",
-        "Yaa",
-        "Afua",
-        "Ama"
-      ];
+    var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-}
+    //fetching data from HTML using their id
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var day = parseInt(document.getElementById("day").value);
+    var name =(document.getElementById("name").value);
+
+};
