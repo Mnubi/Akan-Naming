58976eca23704a5100a77d6451c8e2cedfc697c8
mnubi <rodger.kamau@student.moringaschool.com>
2021-09-05 10:34:55 +0300

    added arrays[weeks,akan names]

---
+++ main.js
@@ -0,0 +1,31 @@
+//possible variables for birth date corresponds with day of the week
+var submit = function(){
+    var DaysofTheWeek = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    var maleNames = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+      ];
+      var femaleNames = [
+        "Akosua",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama"
+      ];
+
+}
